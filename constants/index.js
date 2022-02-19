d106f3a3e94605aef3520b16ba019da76e14534e
mohamad rabi <83583353+mohamadrabi12@users.noreply.github.com>
2022-02-19 23:14:15 +0200

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,8 @@
+import DIMENS from './dimens';
+import SPACING from './spacing';
+import TYPOGRAPHY from './typography';
+import LIMITS from './limits';
+
+export { DIMENS, SPACING, TYPOGRAPHY, LIMITS };
+export * from './actionsTypes';
+export * from './stringsConstants';
